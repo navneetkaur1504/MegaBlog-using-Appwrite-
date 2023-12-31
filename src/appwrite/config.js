fd95dda843bf6729a573852c1dd57fd492805b61
navneetkaur1504 <navneetkaur150403@gmail.com>
2023-12-31 22:05:21 +0530

    updated

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -68,13 +68,64 @@ export class Service{
 
     async getPost(slug){
         try{
-
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            ) 
         } catch(error){
-            console.log("Appwrite service :: createPost :: error", error);
+            console.log("Appwrite service :: getPost :: error", error);
             return false
         }
     }
 
+    async getPosts(queries = [Query.equal("status","active")]){
+        try{
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries
+            )
+        } catch(error){
+            console.log("Appwrite service :: getPosts :: error", error);
+            return false 
+        }
+    }
+
+    //file upload services
+
+    async uploadFile(file){
+        try{
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch(error){
+            console.log("Appwrite service :: uploadFlie :: error", error);
+        return false
+        }
+    }
+
+    async deleteFile(fileId){
+        try{
+            await this.bucket.deleteFile(
+             conf.appwriteBucketId,
+             fileId   
+            )
+            return true
+        } catch(error){
+            console.log("Appwrite service :: deleteFile :: error", error);
+        return false
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
 }
 
 const service = new Service()
