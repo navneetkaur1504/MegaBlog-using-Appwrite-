5376bd0f84963adac53776640bc991ed56845455
navneetkaur1504 <navneetkaur150403@gmail.com>
2024-01-07 01:16:04 +0530

    updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -28,11 +28,11 @@ function Login() {
   return (
     <div className='flex items-center justify-center w-full'>
         <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
-        <div className="mb-2 flex justify-center">
+            <div className="mb-2 flex justify-center">
                     <span className="inline-block w-full max-w-[100px]">
                         <Logo width="100%" />
                     </span>
-        </div>
+            </div>
         <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
         <p className="mt-2 text-center text-base text-black/60">
                 Don&apos;t have any account?&nbsp;
@@ -43,8 +43,45 @@ function Login() {
                         Sign Up
                     </Link>
         </p>
-        {error && <p className='text-red-600 mt-8 text-center'></p>}
-    </div>
+        {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
+
+        <form onSubmit={handleSubmit(login)}
+        className='mt-8'>
+            <div className='space-y-5'>
+
+                /*Input Component*/
+                <Input
+                label="Email: "
+                placeholder="Enter your email"
+                type="email"
+                {...register("email", {
+                    required: true,
+                    validate: {
+                        matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                        "Email address must be a valid address",
+                    }
+                })}
+                />
+                <Input
+                label="Password: "
+                type="password"
+                placeholder="Emter Your password"
+                {...register("password", {
+                    required: true,
+                })}
+                 />
+
+                <Button
+                type="Submit"
+                className="w-full"
+                >Sign in</Button>
+
+
+
+            </div>
+
+        </form>
+        </div>
     </div>
   )
 }
