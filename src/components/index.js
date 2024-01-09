978849e6805aa11473a471a1fd5f460c4f496fd3
navneetkaur1504 <navneetkaur150403@gmail.com>
2024-01-10 00:58:11 +0530

    updated

--- src/components/index.js
+++ src/components/index.js
@@ -4,12 +4,20 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutbBtn from "./Header/LogoutbBtn";
 import RTE from "./RTE";
-
+import Signup from "./Signup";
+import Login from "./Login";
+import Post from "./Post";
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard";
 export {
     Header,
     Footer,
     Container,
     Logo,
     LogoutbBtn,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard
 }
---
+++ src/pages/AllPosts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import appwriteService from "../appwrite/config"
+import { useNavigate } from 'react-router-dom'
+function AllPosts() {
+  return (
+    <div>AllPosts</div>
+  )
+}
+
+export default AllPosts
